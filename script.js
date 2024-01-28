3fce9ae1504f6c827e6a72fb3eb5ff03ea93dec7
Rerisson H R Silva <148692810+Rerisson666@users.noreply.github.com>
2024-01-28 07:38:21 -0300

    alterações aula 4 27/01

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlists')
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
-        .then((Response) => Response.json())
+        .then((response) => response.json())
         .then((result) => displayResults(result))
 }
 
-requestApi(searchTerm)
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
-document.addEventListener('input', function() {
-    const searchTerm = searchInput.value.tolowerCase();
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
-        return;
+        return
     }
+    
+    requestApi(searchTerm);
 })
